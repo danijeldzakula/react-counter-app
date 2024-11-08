ebc9a248bb673c67edb25398638b2da3e75f5469
danijeldzakula <danidzaki060@gmail.com>
2024-11-09 00:15:12 +0100

    feat: implement react router dom v6

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,56 @@
-import { Counter } from "./components/counter";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import RootLayout from "./components/layout/RootLayout";
+import AuthLayout from "./components/layout/AuthLayout";
+import Home from "./app/Home";
+import Login from "./app/auth/Login";
+import { Outlet } from "react-router-dom";
+import About from "./app/about/About";
 
 export default function App() {
-  return <Counter />;
-}
+  const router = createBrowserRouter(
+    [
+      {
+        path: "/",
+        element: <Outlet />,
+        children: [
+          {
+            element: <RootLayout />,
+            children: [
+              {
+                index: true,
+                element: <Home />
+              },
+              {
+                path: 'about',
+                element: <About />
+              }
+            ]
+          },
+          {
+            element: <AuthLayout />,
+            children: [
+              {
+                path: "login",
+                element: <Login />,
+              },
+            ],
+          },
+        ],
+      },
+    ],
+    {
+      future: {
+        v7_fetcherPersist: true,
+      },
+    }
+  );
 
+  return (
+    <RouterProvider 
+      router={router} 
+      future={{
+        v7_startTransition: true,
+      }}
+    />
+  )
+}
